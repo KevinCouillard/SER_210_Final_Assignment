369fdd60e1b5efdb42ec8e04906b3f65b4966d93
KevinCouillard <kevincouillard48@gmail.com>
2021-04-13 14:55:32 -0400

    Second Push (Base w/ empty activities) - Kevin Couillard

---
+++ app/src/main/java/edu/quinnipiac/ser210/ser_210_final_assignment/InfoActivity.java
@@ -0,0 +1,14 @@
+package edu.quinnipiac.ser210.ser_210_final_assignment;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class InfoActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_info);
+    }
+}
