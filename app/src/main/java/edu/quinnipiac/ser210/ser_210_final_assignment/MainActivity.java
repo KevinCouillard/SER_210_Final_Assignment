4f4fc52d430a6373baafe3f690416c2c1ef878a8
KevinCouillard <kevincouillard48@gmail.com>
2021-04-14 22:30:18 -0400

    Created Basic Layouts (Not Complete) - Kevin Couillard

--- app/src/main/java/edu/quinnipiac/ser210/ser_210_final_assignment/MainActivity.java
+++ app/src/main/java/edu/quinnipiac/ser210/ser_210_final_assignment/MainActivity.java
@@ -2,7 +2,9 @@ package edu.quinnipiac.ser210.ser_210_final_assignment;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +12,10 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void startApp(View view) {
+        Intent intent = new Intent(this,ServicesActivity.class);
+        startActivity(intent);
     }
 }
--- app/src/main/java/edu/quinnipiac/ser210/ser_210_final_assignment/ServicesActivity.java
+++ app/src/main/java/edu/quinnipiac/ser210/ser_210_final_assignment/ServicesActivity.java
@@ -2,7 +2,9 @@ package edu.quinnipiac.ser210.ser_210_final_assignment;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class ServicesActivity extends AppCompatActivity {
 
@@ -10,5 +12,10 @@ public class ServicesActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_services);
+    }
+
+    public void start_options(View view) {
+        Intent intent = new Intent(this, OptionsActivity.class);
+        startActivity(intent);
     }
 }
