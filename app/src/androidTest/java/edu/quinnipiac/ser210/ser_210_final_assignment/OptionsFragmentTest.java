f9f8a2f6503d72f894e839226a142a980f0d0f48
KevinCouillard <kevincouillard48@gmail.com>
2021-05-01 00:34:44 -0400

    UI Test Changes (error) - Kevin Couillard

--- app/src/androidTest/java/edu/quinnipiac/ser210/ser_210_final_assignment/OptionsFragmentTest.java
+++ app/src/androidTest/java/edu/quinnipiac/ser210/ser_210_final_assignment/OptionsFragmentTest.java
@@ -1,46 +1,77 @@
 package edu.quinnipiac.ser210.ser_210_final_assignment;
 
-import android.view.View;
+import android.content.Context;
+import android.content.Intent;
 
-import androidx.fragment.app.testing.FragmentScenario;
-import androidx.navigation.Navigation;
-import androidx.navigation.testing.TestNavHostController;
-import androidx.test.core.app.ApplicationProvider;
-import androidx.test.espresso.action.ViewActions;
-import androidx.test.espresso.matcher.ViewMatchers;
-import androidx.test.ext.junit.runners.AndroidJUnit4;
+import androidx.test.core.app.ActivityScenario;
+
+        import androidx.test.ext.junit.runners.AndroidJUnit4;
+import androidx.test.platform.app.InstrumentationRegistry;
+import androidx.test.rule.ActivityTestRule;
+
+
+import org.junit.Rule;
 import org.junit.Test;
-import org.junit.runner.RunWith;
 
-import static androidx.test.espresso.Espresso.onView;
-import static androidx.test.espresso.action.ViewActions.click;
-import static androidx.test.espresso.assertion.ViewAssertions.matches;
-import static androidx.test.espresso.matcher.ViewMatchers.isDisplayed;
-import static androidx.test.espresso.matcher.ViewMatchers.withId;
-import static org.junit.Assert.*;
+        import org.junit.runner.RunWith;
+
+
+
+        import static androidx.test.espresso.Espresso.onView;
+
+        import static androidx.test.espresso.action.ViewActions.click;
+
+        import static androidx.test.espresso.action.ViewActions.closeSoftKeyboard;
+
+        import static androidx.test.espresso.action.ViewActions.typeText;
+
+        import static androidx.test.espresso.assertion.ViewAssertions.matches;
+
+        import static androidx.test.espresso.matcher.ViewMatchers.isDisplayed;
+
+        import static androidx.test.espresso.matcher.ViewMatchers.withId;
+import static androidx.test.espresso.matcher.ViewMatchers.withText;
+
+
 @RunWith(AndroidJUnit4.class)
+
 public class OptionsFragmentTest {
 
+    @Rule
+    public ActivityTestRule<OptionsActivity> oActivityRule =
+            new ActivityTestRule<OptionsActivity>(OptionsActivity.class) {
+                @Override
+                protected Intent getActivityIntent() {
+                    Context targetContext = InstrumentationRegistry.getInstrumentation().getTargetContext();
+                    Intent result = new Intent(targetContext, OptionsActivity.class);
+                    result.putExtra("placeTypeSelected", 0);
+                    return result;
+                }
+            };
+
     @Test
-    public void onClick() {
-        TestNavHostController navController = new TestNavHostController(
-                ApplicationProvider.getApplicationContext());
 
-        FragmentScenario<OptionsFragment> optionsFragmentFragmentScenario = FragmentScenario.launchInContainer(OptionsFragment.class);
-        navController.setGraph(R.navigation.nav_map);
+    public void UITest() {
+
+        ActivityScenario<MainActivity> activityScenario = ActivityScenario.launch(MainActivity.class);
+        ActivityScenario<OptionsActivity> activityScenario2 = ActivityScenario.launch(OptionsActivity.class);
 
 
-        optionsFragmentFragmentScenario.onFragment(fragment ->
-        // Make the NavController available via the findNavController() APIs
-        Navigation.setViewNavController(fragment.requireView(), navController)
-        );
 
-        //onView(withId(R.id.option_1)).perform(click());
+        //Click on button
 
-        //This is the line from the developers page (The currentDestination method is giving error)
-        //assertThat(navController.currentDestination.id).isEqualTo(R.id.info_frag);
+        onView(withId(R.id.option_1)).perform(click());
 
-       // assertThat(navController.getCurrentDestination().getId(),matches(View.info_frag));
-        //onView(withId(R.id.info_frag)).check(matches(isDisplayed()));
+
+
+        //Check that the destination fragment being displayed is
+
+        onView(withId(R.id.info_frag)).check(matches(isDisplayed()));
+
+        //onView(withId(R.id.info_text)).check(matches(isDisplayed()));
+
+
+
+            }
+
     }
-}
